21f83505c7404d8500f3ba98d8250d80c500217e
Amaburi <drsservant@gmail.com>
2023-12-16 22:56:35 +0700

    Auth done.

--- src/app/(pages)/recover-password/page.tsx
+++ src/app/(pages)/recover-password/page.tsx
@@ -6,12 +6,37 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import { RecoverPasswordForm } from './RecoverPasswordForm'
 
 import classes from './index.module.scss'
-
+import Link from 'next/link'
+import Image from 'next/image'
+import { RenderParams } from '../../_components/RenderParams'
 export default async function RecoverPassword() {
   return (
-    <Gutter className={classes.recoverPassword}>
-      <RecoverPasswordForm />
-    </Gutter>
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt='logo'
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params}/>
+          <Link href='/login' className={classes.backLink}>
+            <Image src="/assets/icons/arrow-left.svg" alt='back button' width={24} height={24}/>
+          </Link>
+          <div className={classes.formTitle}>
+            <h3>Recover Your Password</h3>
+          </div>
+          <RecoverPasswordForm/>
+        </div>
+      </div>
+    </section>
   )
 }
 
